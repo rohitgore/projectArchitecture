070e9bb452f67afd28c86f0928d848c858b16109
rohitgore <rrohit.gore@gmail.com>
2023-01-27 20:31:38 +0530

    added application configuration

---
+++ src/app/Services/apicalling.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApicallingService {
+ 
+  constructor(private http:HttpClient) { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+// Get API calling
+  apiGet(url:any):Observable<any>
+{
+   return this.http.get<any>(url).pipe(retry(1), catchError(this.handleError));
+}
+
+// Error handling
+handleError(error: any) {
+  let errorMessage = '';
+  if (error.error instanceof ErrorEvent) {
+    // Get client-side error
+    errorMessage = error.error.message;
+  } else {
+    // Get server-side error
+    errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+  }
+  window.alert(errorMessage);
+  return throwError(() => {
+    return errorMessage;
+  });
+}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApicallingService } from './Services/apicalling.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private ap:ApicallingService ){}
   title = 'setupApplication';
+  url = 'http://localhost:3000/Person';
+
+  getdata()
+  {
+    this.ap.apiGet(this.url).subscribe(
+      res=>{
+        console.log(res);
+      }
+    )
+  }
+
 }
